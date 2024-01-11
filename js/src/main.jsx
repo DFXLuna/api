a4c8392a68e366f2080efac7b4a65e6c2c3ced3c
Emilia <eg@dfxluna.xyz>
2024-01-11 16:16:18 -0700

    pretty frontend

--- js/src/main.jsx
+++ js/src/main.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import App from './app.jsx'
+import Navbar from './navbar.jsx'
 
 console.log(`Using VITE_API_HOST=${import.meta.env.VITE_API_HOST}`)
 console.log(`Mode: ${import.meta.env.MODE}`)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Navbar />
     <App apiHost={import.meta.env.VITE_API_HOST} />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
---
+++ js/src/navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+    return (
+        <nav className='navbar sticky bg-base-300 rounded-xl'>
+            <a className='btn btn-ghost text-xl font-serif font-light'>something</a>
+        </nav>
+    )
+}
+
+export default Navbar
